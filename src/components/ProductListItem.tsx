19ea90d558f0b32db3acb6730e086919977da9aa
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-12 13:35:04 +0200

    asChild: We want the whole component to be clickable, when we wrap the link with it, the styles are broken, use asChild attr on the Link to show that it should act like a child, then the styles are back

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -12,16 +12,17 @@ type ProductListItemProps = {
 
 export const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image || defaultPizzaImage }}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-      <Link href={"/product"}>Go to details</Link>
-    </View>
+    <Link href={"/product"} asChild>
+      <View style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </View>
+    </Link>
   );
 };
 
