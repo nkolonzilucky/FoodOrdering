af0dc296cdf091e0c5842e3012c4bd6c5badc646
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-12 21:03:17 +0200

    Now we specified the title of a page on the actual component instead at the _layout.tsx

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,12 +1,13 @@
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
 const ProductDetailsScreen = () => {
-  const { id } = useLocalSearchParams()
+  const { id } = useLocalSearchParams();
   return (
     <View>
-      <Text style={{fontSize: 20}}>ProductDetailsScreen for id: { id }</Text>
+      <Stack.Screen options={{ title: "Details" }} />
+      <Text style={{ fontSize: 20 }}>ProductDetailsScreen for id: {id}</Text>
     </View>
   );
 };
