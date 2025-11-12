5b184eb315e96929a281cd5e0c5e7f4d07deeaba
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-12 12:38:42 +0200

    added padding inside the contentContainerStyle

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ export default function MenuScreen() {
       data={products}
       renderItem={({ item }) => <ProductListItem product={item} />}
       numColumns={2}
-      contentContainerStyle={{ gap: 10 }}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
       columnWrapperStyle={{ gap: 10 }}
     />
   );
