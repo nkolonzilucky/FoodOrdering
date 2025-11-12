33a29b9376fd789ff451635349d341f45836f46a
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-12 03:20:55 +0200

    added an image using the uri object as the source value, changed styles, used a dynamic value for data such as product name, price and uri

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import Colors from "@/constants/Colors";
+import { Image, StyleSheet, Text, View } from "react-native";
+import products from "../../../assets/data/products";
+
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Pizza Peperoni</Text>
-      <Text>$12.99</Text>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
   },
   title: {
-    fontSize: 20,
-    fontWeight: "bold",
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1, //Height will be auto calculated based on the width, 1 means squre. 2/1 means height is half the width
   },
 });
