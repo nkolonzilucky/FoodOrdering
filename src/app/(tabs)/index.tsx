811c9b8eb2501c895c1db9b388d5932ed84e4005
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-12 04:10:02 +0200

    This commit shows that components are actually functions.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,12 +4,21 @@ import products from "../../../assets/data/products";
 
 const product = products[0];
 
-export default function TabOneScreen() {
+const ProductListItem = () => {
   return (
     <View style={styles.container}>
       <Image source={{ uri: product.image }} style={styles.image} />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default function TabOneScreen() {
+  return (
+    <View>
+      <ProductListItem />
+      <ProductListItem />
     </View>
   );
 }
