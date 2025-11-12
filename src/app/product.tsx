b09c5b32b8f2c226481d98980d491226b354fdd1
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-12 13:09:21 +0200

    rnfe: shotcut to create a react-native component

---
+++ src/app/product.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const product = () => {
+  return (
+    <View>
+      <Text>product</Text>
+    </View>
+  )
+}
+
+export default product
