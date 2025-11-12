9466d5f3f7b5f24c371103a47c22166becaa0863
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-12 22:27:05 +0200

    added the product details: image and price and style on the container.

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,39 @@
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import products from "@assets/data/products";
 import { Stack, useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id.toString() === id);
+  if (!product) return <Text>Product not found</Text>;
   return (
-    <View>
-      <Stack.Screen options={{ title: "Details " + id }} />
-      <Text style={{ fontSize: 20 }}>ProductDetailsScreen for id: {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
 
 export default ProductDetailsScreen;
