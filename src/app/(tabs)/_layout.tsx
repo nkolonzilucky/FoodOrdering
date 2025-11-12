2a609d3d2e6914857e75b0e3b53a9e6644378cd8
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-12 20:03:13 +0200

    changed the tab.screen name from index to menu

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -28,7 +28,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="menu"
         options={{
           title: "Menu",
           tabBarIcon: ({ color }) => (
