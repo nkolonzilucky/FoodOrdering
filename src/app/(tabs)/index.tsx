e5a1454cd77d7802f232637da373a0041657eb76
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-12 04:23:46 +0200

    we have now extracted the component function to its own .tsx file under the components folder

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,50 +1,12 @@
-import Colors from "@/constants/Colors";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { ProductListItem } from "@/components/ProductListItem";
+import { View } from "react-native";
 import products from "../../../assets/data/products";
 
-const product = products[0];
-
-const ProductListItem = () => {
-  return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
-  );
-};
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem />
-      <ProductListItem />
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1, //Height will be auto calculated based on the width, 1 means squre. 2/1 means height is half the width
-  },
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,38 @@
+import Colors from "@/constants/Colors";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+export const ProductListItem = ({ product }: { product: any }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1, //Height will be auto calculated based on the width, 1 means squre. 2/1 means height is half the width
+  },
+});
