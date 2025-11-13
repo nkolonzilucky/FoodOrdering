a6530533125f7755691ef101fd111fca4a75ca50
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-14 00:32:04 +0200

    total: We started the implementation of the total in the CartProvider, then imported it in the cart.tsx

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,10 +3,10 @@ import CartListItem from "@/components/CartListItem";
 import { useCart } from "@/providers/CartProvider";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { FlatList, Platform, View } from "react-native";
+import { FlatList, Platform, Text, View } from "react-native";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
   return (
     <View style={{ padding: 10 }}>
       <FlatList
@@ -14,7 +14,9 @@ const CartScreen = () => {
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         contentContainerStyle={{ gap: 10 }}
       />
-
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "500" }}>
+        Total: ${total}
+      </Text>
       <Button text="Checkout" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
