00a875f2e021f252a143cf790238df6a0a2226c0
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-14 01:21:03 +0200

    Changed the color of the icons to be readable

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -20,7 +20,8 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].background,
+        tabBarInactiveTintColor: "gainsboro",
         tabBarStyle: { backgroundColor: Colors.light.tint },
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
