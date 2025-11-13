587a3c66bd1588af5d77b6a25757d09827ec8d49
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-13 15:43:29 +0200

    We have defined the items and the addItem 'values' inside the CartProvider. And have shown that CartContext will return the CartType we just created.

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,16 +1,19 @@
 import { CartItem, PizzaSize, Product } from "@/types";
-import { createContext, PropsWithChildren, useContext } from "react";
+import { createContext, PropsWithChildren, useContext, useState } from "react";
 
 type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: PizzaSize) => void;
 };
 
-const CartContext = createContext({});
+const CartContext = createContext<CartType>({});
 
 const CartProvider = ({ children }: PropsWithChildren) => {
+  const [items, setItems] = useState<CartItem[]>([]);
+  const addItem = (product: Product, size: PizzaSize) => {};
+
   return (
-    <CartContext.Provider value={{ items: [1, 2, 3], onAddItem: () => {} }}>
+    <CartContext.Provider value={{ items, addItem }}>
       {children}
     </CartContext.Provider>
   );
