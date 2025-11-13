642d23d752e6f75bdfc530f33c1495fede778fa4
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-13 17:16:52 +0200

    cartItem: Now the cart shows the added items, and it is very beautiful!

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,13 +1,17 @@
+import CartListItem from "@/components/CartListItem";
 import { useCart } from "@/providers/CartProvider";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Platform, Text, View } from "react-native";
+import { FlatList, Platform, View } from "react-native";
 
 const CartScreen = () => {
   const { items } = useCart();
   return (
     <View>
-      <Text>Cart Items length: {items.length}</Text>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+      />
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
