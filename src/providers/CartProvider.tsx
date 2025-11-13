230aa8850bbf83b633cfec38c61487c899cb9d80
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-13 15:30:19 +0200

    useCart: No need to export the CartContext anymore

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
 
-export const CartContext = createContext({});
+const CartContext = createContext({});
 
 const CartProvider = ({ children }) => {
   return (
