6e77d0081cf9f3127b3070cab70832a51966f646
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-13 15:31:07 +0200

    PropsWithChildren: to give the children prop a type

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,8 +1,8 @@
-import { createContext, useContext } from "react";
+import { createContext, PropsWithChildren, useContext } from "react";
 
 const CartContext = createContext({});
 
-const CartProvider = ({ children }) => {
+const CartProvider = ({ children }: PropsWithChildren) => {
   return (
     <CartContext.Provider value={{ items: [1, 2, 3], onAddItem: () => {} }}>
       {children}
