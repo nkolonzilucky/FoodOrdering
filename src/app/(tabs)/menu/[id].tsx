96ed7c7ded37698a714685b0adfe260cc4837323
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-13 16:13:22 +0200

    addItem: implemented

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,21 +1,26 @@
 import Button from "@/components/Button";
 import { defaultPizzaImage } from "@/components/ProductListItem";
+import { useCart } from "@/providers/CartProvider";
+import { PizzaSize } from "@/types";
 import products from "@assets/data/products";
 import { Stack, useLocalSearchParams } from "expo-router";
 import React, { useState } from "react";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
-const sizes = ["S", "M", "L", "XL"];
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
-  const [selectedSize, setSelectedSize] = useState("M");
+  const { addItem } = useCart();
+
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
 
   const product = products.find((p) => p.id.toString() === id);
   if (!product) return <Text>Product not found</Text>;
 
   const addToCart = () => {
-    console.warn("Adding to cart ", selectedSize);
+    if (!product) return;
+    addItem(product, selectedSize);
   };
 
   return (
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -13,7 +13,16 @@ const CartContext = createContext<CartType>({
 
 const CartProvider = ({ children }: PropsWithChildren) => {
   const [items, setItems] = useState<CartItem[]>([]);
-  const addItem = (product: Product, size: PizzaSize) => {};
+  const addItem = (product: Product, size: PizzaSize) => {
+    const newCartItem: CartItem = {
+      id: "1",
+      product,
+      product_id: product.id,
+      size,
+      quantity: 1,
+    };
+    setItems([newCartItem, ...items]);
+  };
 
   return (
     <CartContext.Provider value={{ items, addItem }}>
