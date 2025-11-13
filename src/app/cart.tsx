56ae1b9e012b42e2db05d37af0ea0299f77dd0fc
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-13 09:25:43 +0200

    We can now remove the old modal, we only copied the StatusBar styling.

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Platform, Text, View } from "react-native";
 
 const CartScreen = () => {
   return (
     <View>
       <Text>CartScreen</Text>
+      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
