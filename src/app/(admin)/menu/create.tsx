ff8d90f6b5ed03bacf21e2e705b158a161b2ec08
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-14 13:30:57 +0200

    Added an image placeholder with a label

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,67 +1,70 @@
 import Button from "@/components/Button";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import Colors from "@/constants/Colors";
 import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
 
 const CreateProductScreen = () => {
   const [name, setName] = useState("");
-    const [price, setPrice] = useState("");
+  const [price, setPrice] = useState("");
 
-    const [errors, setErrors] = useState("");
+  const [errors, setErrors] = useState("");
 
-    const resetFields = () => {
-      setName("");
-      setPrice("");
-    };
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
 
-    const validateInput = () => {
-      setErrors("");
-      if (!name) {
-        setErrors("Name is required");
-        return false;
-      }
-      if (!price) {
-        setErrors("Price is required");
-        return false;
-      }
+  const validateInput = () => {
+    setErrors("");
+    if (!name) {
+      setErrors("Name is required");
+      return false;
+    }
+    if (!price) {
+      setErrors("Price is required");
+      return false;
+    }
 
-      if (isNaN(parseFloat(price))) {
-        setErrors("Price is not a number");
-        return false;
-      }
-      return true;
-    };
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price is not a number");
+      return false;
+    }
+    return true;
+  };
 
-    const onCreate = () => {
-      if (!validateInput()) {
-        return;
-      }
-      console.warn("Creating product");
-      //Save in the database
-      resetFields();
-    };
+  const onCreate = () => {
+    if (!validateInput()) {
+      return;
+    }
+    console.warn("Creating product");
+    //Save in the database
+    resetFields();
+  };
 
-    return (
-      <View style={styles.container}>
-        <Text style={styles.label}>create</Text>
-        <TextInput
-          value={name}
-          onChangeText={setName}
-          placeholder="Name"
-          style={styles.input}
-        />
-        <Text style={styles.label}>price ($)</Text>
-        <TextInput
-          value={price}
-          onChangeText={setPrice}
-          placeholder="9.99"
-          style={styles.input}
-          keyboardType="numeric"
-        />
-        <Text style={{ color: "red" }}>{errors}</Text>
-        <Button onPress={onCreate} text="Create" />
-      </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: defaultPizzaImage }} style={styles.image} />
+      <Text style={styles.textButton}>Select an image</Text>
+      <Text style={styles.label}>create</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+        style={styles.input}
+      />
+      <Text style={styles.label}>price ($)</Text>
+      <TextInput
+        value={price}
+        onChangeText={setPrice}
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType="numeric"
+      />
+      <Text style={{ color: "red" }}>{errors}</Text>
+      <Button onPress={onCreate} text="Create" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -70,6 +73,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 10,
     gap: 10,
+  },
+  image: {
+    width: "50%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
   },
   input: {
     backgroundColor: "white",
