5a5326354472af9eddc4fa7153203b52098b2b73
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-14 22:14:17 +0200

    Implemented the sign-in screen

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,12 +1,63 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import Button from "@/components/Button";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const SignInScreen = () => {
-  return (
-    <View>
-      <Text>SignInScreen</Text>
-    </View>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default SignInScreen
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="jon@gmail.com"
+        style={styles.input}
+      />
+
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder=""
+        style={styles.input}
+        secureTextEntry
+      />
+
+      <Button text="Sign in" />
+      <Link href={"/sign-up"} style={styles.textButton}>
+        Create an account
+      </Link>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+  },
+  label: {
+    color: "gray",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 20,
+    backgroundColor: "white",
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+  },
+});
+
+export default SignInScreen;
