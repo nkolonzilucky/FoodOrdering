00db84097b65c9856cc19571b085238906fe2631
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-14 10:12:08 +0200

    Included the Stack.screen inside the containing View element

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,22 +5,15 @@ import { View } from "react-native";
 
 const index = () => {
   return (
-    <>
-      <Stack.Screen
-        options={{
-          headerShown: false,
-        }}
-      />
-
-      <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
-        <Link href={"/(user)"} asChild>
-          <Button text="User" />
-        </Link>
-        <Link href={"/(admin)"} asChild>
-          <Button text="Admin" />
-        </Link>
-      </View>
-    </>
+    <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+      <Stack.Screen options={{ headerShown: false }} />
+      <Link href={"/(user)"} asChild>
+        <Button text="User" />
+      </Link>
+      <Link href={"/(admin)"} asChild>
+        <Button text="Admin" />
+      </Link>
+    </View>
   );
 };
 
