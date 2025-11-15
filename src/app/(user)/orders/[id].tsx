daeec9eb83eaf73c242befa129dd9e354750a0e6
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-11-15 19:18:03 +0200

    added some initial order details logic

--- src/app/(user)/orders/[id].tsx
+++ src/app/(user)/orders/[id].tsx
@@ -1,15 +1,63 @@
-import { useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { Text, View } from 'react-native';
+import { Order } from "@/types";
+import orders from "@assets/data/orders";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { Stack, useLocalSearchParams } from "expo-router";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-const Order = () => {
-    const { id } = useLocalSearchParams();
-    console.log('Order #', id)
+dayjs.extend(relativeTime);
+
+const OrderScreen = () => {
+  const { id } = useLocalSearchParams();
+  const order = orders.find((order) => order.id.toString() === id);
   return (
-    <View>
-      <Text>Order</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: `Order #${id}` }} />
+      <View style={styles.detailsContainer}>
+        <View style={styles.orderAndTime}>
+          <Text style={styles.orderNumber}> Order #{id}</Text>
+          <Text style={styles.duration}>
+            {dayjs(order?.created_at).fromNow()}
+          </Text>
+        </View>
+        <Text>{order?.status}</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Order
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+    backgroundColor: "white",
+  },
+  orderAndTime: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 6,
+  },
+  orderNumber: {
+    fontWeight: "bold",
+    color: "black",
+    fontSize: 16,
+  },
+  duration: {
+    color: "gray",
+    fontSize: 12,
+  },
+  status: {
+    fontSize: 500,
+    color: "black",
+  },
+});
+
+export default Order;
--- src/app/(user)/orders/_layout.tsx
+++ src/app/(user)/orders/_layout.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 const OrdersLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name='orders' options={{title: 'Orders'}} />
+      <Stack.Screen name="orders" options={{ title: "Orders" }} />
     </Stack>
-  )
+  );
 }
 
 export default OrdersLayout
