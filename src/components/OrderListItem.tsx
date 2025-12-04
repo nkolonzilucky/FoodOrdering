f4cd3a9de2669d6b00ff6c0637c4508e1940b1d1
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-12-04 05:46:10 +0200

    implemented time display from today

--- src/components/OrderListItem.tsx
+++ src/components/OrderListItem.tsx
@@ -1,6 +1,10 @@
 import { Order } from "@/types";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
+dayjs.extend(relativeTime);
 
 type OrderListItemProps = {
   order: Order;
@@ -11,34 +15,37 @@ const OrderListItem = ({ order }: OrderListItemProps) => {
     <View style={styles.container}>
       <View style={styles.orderStatus}>
         <Text style={styles.order}>Order# {order.id}</Text>
-        <Text style={styles.timestamp}>{order.created_at}</Text>
+        <Text style={styles.timestamp}>{dayjs().to(order.created_at)}</Text>
       </View>
-      <Text>{order.status}</Text>
+      <Text style={styles.status}>{order.status}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent:'space-between',
-        backgroundColor: 'white',
-        borderRadius: 12,
-        padding: 10,
-        marginBottom: 10,
-    },
-    orderStatus: {
-        flex: 1,
-        gap:6
-    },
-    order: {
-        fontWeight: 'bold',
-        color: 'black'
-    },
-    timestamp: {
-        color: 'gray'
-    }
-})
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    borderRadius: 12,
+    padding: 10,
+    marginBottom: 10,
+  },
+  orderStatus: {
+    flex: 1,
+    gap: 6,
+  },
+  order: {
+    fontWeight: "bold",
+    color: "black",
+  },
+  timestamp: {
+    color: "gray",
+  },
+  status: {
+    fontWeight: "600",
+  },
+});
 export default OrderListItem;
