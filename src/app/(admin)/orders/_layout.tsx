bc4806b0a0b17000e7817a02d299db3ce71bf02c
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-12-16 12:36:22 +0200

    added a safe area

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const OrdersStack = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Orders" }} />
+      <Stack.Screen name="list" options={{ headerShown: false }} />
       <Stack.Screen name="[id]" options={{ title: "Orders Details" }} />
     </Stack>
   );
---
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -0,0 +1,16 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { withLayoutContext } from "expo-router";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const TopTabs = withLayoutContext(createMaterialTopTabNavigator().Navigator);
+
+const OrderListNavigator = () => {
+  return (
+    <SafeAreaView edges={['top']} style={{flex:1, backgroundColor: 'white'}}>
+      <TopTabs />
+    </SafeAreaView>
+  );
+};
+
+export default OrderListNavigator;
