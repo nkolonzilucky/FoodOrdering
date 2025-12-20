6ea69fe882184be09dea071ebd09f83c0826b980
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-12-20 10:28:33 +0200

    implemnted a sign-up

--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,12 +1,23 @@
 import Button from "@/components/Button";
 import Colors from "@/constants/Colors";
+import { supabase } from "@/lib/supabase";
 import { Link } from "expo-router";
 import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, StyleSheet, Text, TextInput, View } from "react-native";
 
 const SignUpScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loeading, setLoading] = useState(false);
+  async function signUpWithEmail() {
+    setLoading(true);
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Email</Text>
@@ -25,7 +36,11 @@ const SignUpScreen = () => {
         secureTextEntry
       />
 
-      <Button text="Create account" />
+      <Button
+        onPress={signUpWithEmail}
+        disabled={loeading}
+        text={loeading ? "Creating account..." : "Create account"}
+      />
       <Link href={"/sign-in"} style={styles.textButton}>
         Sign in
       </Link>
---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import { createClient } from '@supabase/supabase-js';
+import * as SecureStore from 'expo-secure-store';
+import 'react-native-url-polyfill/auto';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://jfzenbghajquofnadcni.supabase.co';
+const supabaseAnonKey = 'sb_publishable_AuQYRZ27qtqjGC3Qo6cPEA_O0bApmxk';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
