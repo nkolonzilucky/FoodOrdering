8e4e684be24723e015353bdb6b9ad7da2916d6ee
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-12-22 10:52:52 +0200

    Protected the auth layout

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,13 +1,19 @@
-import { Stack } from 'expo-router'
-import React from 'react'
+import { useAuth } from "@/providers/AuthProvider";
+import { Redirect, Stack } from "expo-router";
+import React from "react";
 
 const AuthStack = () => {
+  const { session } = useAuth();
+
+  if (session) {
+    return <Redirect href={"/"} />;
+  }
   return (
     <Stack>
       <Stack.Screen name="sign-in" options={{ title: "Sign In" }} />
       <Stack.Screen name="sign-up" options={{ title: "Sign Up" }} />
     </Stack>
   );
-}
+};
 
-export default AuthStack
+export default AuthStack;
