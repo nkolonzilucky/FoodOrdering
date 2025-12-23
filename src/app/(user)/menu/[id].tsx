921f6e2c9ce46146ac886f8565de94f6a2cb770e
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-12-23 21:22:53 +0200

    fetched a product using id

--- src/app/(user)/menu/[id].tsx
+++ src/app/(user)/menu/[id].tsx
@@ -1,8 +1,8 @@
+import { useProduct } from "@/api/products";
 import Button from "@/components/Button";
 import { defaultPizzaImage } from "@/components/ProductListItem";
 import { useCart } from "@/providers/CartProvider";
 import { PizzaSize } from "@/types";
-import products from "@assets/data/products";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import React, { useState } from "react";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
@@ -10,14 +10,17 @@ import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
-  const { id } = useLocalSearchParams();
+  const { id: idString } = useLocalSearchParams();
+  const id = parseFloat(typeof idString === "string" ? idString : idString[0]);
+
+  const { data: product, error, isLoading } = useProduct(id);
+
   const { addItem } = useCart();
 
   const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
 
   const router = useRouter();
 
-  const product = products.find((p) => p.id.toString() === id);
   if (!product) return <Text>Product not found</Text>;
 
   const addToCart = () => {
