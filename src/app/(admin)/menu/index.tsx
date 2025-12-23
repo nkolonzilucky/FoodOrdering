19949244ce34eaf276733c4c04f94b35e9f35857
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-12-23 09:54:51 +0200

    added useEffect code to fetch products

--- src/app/(admin)/menu/index.tsx
+++ src/app/(admin)/menu/index.tsx
@@ -1,8 +1,18 @@
 import { ProductListItem } from "@/components/ProductListItem";
+import { supabase } from "@/lib/supabase";
 import products from "@assets/data/products";
+import { useEffect } from "react";
 import { FlatList } from "react-native";
 
 export default function MenuScreen() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data, error } = await supabase.from("products").select("*");
+      console.log("fetchProduct", data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <FlatList
       data={products}
