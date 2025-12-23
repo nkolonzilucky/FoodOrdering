776afb156177a4f835e952b40e69c8d7bf33b979
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-12-23 17:56:27 +0200

    created a cusom hook named useProductList

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,31 +1,9 @@
+import { useProductList } from "@/api/products";
 import { ProductListItem } from "@/components/ProductListItem";
-import { supabase } from "@/lib/supabase";
-import { useQuery } from "@tanstack/react-query";
 import { ActivityIndicator, FlatList, Text } from "react-native";
 
 export default function MenuScreen() {
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     const { data, error } = await supabase.from("products").select("*");
-  //     console.log("fetchProduct", data);
-  //   };
-  //   fetchProducts();
-  // }, []);
-
-  const {
-    data: products,
-    error,
-    isLoading,
-  } = useQuery({
-    queryKey: ["products"],
-    queryFn: async () => {
-      const { data, error } = await supabase.from("products").select("*");
-      if (error) {
-        throw new Error(error.message);
-      }
-      return data;
-    },
-  });
+  const { data: products, error, isLoading } = useProductList();
 
   if (isLoading) {
     return <ActivityIndicator />;
