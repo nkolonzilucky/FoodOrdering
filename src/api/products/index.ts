c63565b3b179b88e0992980115c69bf17ab16c71
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-12-24 22:26:13 +0200

    added the if error code on the useDeleteProduct hook.

--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -74,7 +74,10 @@ export const useDeleteProduct = () => {
    const queryClient = useQueryClient()
   return useMutation({
     async mutationFn(id: number) { 
-      const { error } = await supabase.from('products').delete().eq('id',id)
+      const { error } = await supabase.from('products').delete().eq('id', id);
+      if (error) {
+        throw new Error(error.message)
+      }
     },
     async onSuccess() {
       await queryClient.invalidateQueries({queryKey: ['products']})
