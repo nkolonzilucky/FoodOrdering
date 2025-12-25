7d976210f2fa4dc126a20dab9efd8d031b529c77
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-12-25 11:36:35 +0200

    Addede the fetch orders for a specific user

--- src/api/orders/index.ts
+++ src/api/orders/index.ts
@@ -1,4 +1,5 @@
 import { supabase } from "@/lib/supabase";
+import { useAuth } from "@/providers/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
 
 export const useAdminOrderList = () => {
@@ -12,4 +13,23 @@ export const useAdminOrderList = () => {
       return data;
     },
   });
-}
+};
+
+export const useMyOrderList = () => {
+  const { session } = useAuth();
+  const id = session?.user.id;
+  return useQuery({
+    queryKey: ["orders", { userId: id }],
+    queryFn: async () => {
+      if (!id) return null;
+      const { data, error } = await supabase
+        .from("orders")
+        .select("*")
+        .eq("user_id", id);
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    },
+  });
+};
